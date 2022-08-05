1481e5108f7deeb38b4d0c7ca1888eb3ae0ec5dc
Alex Yatsenko <yatsenkolesh@gmail.com>
2022-08-06 00:14:04 +0100

    Initial version (#1)

---
+++ tests/Unit/Collections/RelatedUsersCollectionTest.php
@@ -0,0 +1,319 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Abrouter\RelatedUsers\Tests\Unit\Collections;
+
+use Abrouter\RelatedUsers\Collections\RelatedUsersCollection;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class RelatedUsersCollectionTest extends BaseTestCase
+{
+    public function testAppendingWithSingleUser()
+    {
+        $collection = new RelatedUsersCollection();
+        $collection->append('user', 'relatedUser');
+
+        $this->assertEquals($collection->getAll(), [
+            'user' => ['relatedUser'],
+            'relatedUser' => ['user'],
+        ]);
+    }
+
+    public function testAppendingWithMultipleUsers()
+    {
+        $collection = new RelatedUsersCollection();
+        $collection->append('user', 'relatedUser');
+        $collection->append('user', 'relatedUser2');
+        $collection->append('user', 'relatedUser3');
+
+        $this->assertEquals($collection->getAll(), [
+            'user' => ['relatedUser', 'relatedUser2', 'relatedUser3'],
+            'relatedUser' => ['user', 'relatedUser2', 'relatedUser3'],
+            'relatedUser2' => ['user', 'relatedUser', 'relatedUser3'],
+            'relatedUser3' => ['user', 'relatedUser', 'relatedUser2'],
+        ]);
+    }
+
+    public function testAppendingWithSameUsers()
+    {
+        $collection = new RelatedUsersCollection();
+        $collection->append('user', 'relatedUser');
+        $collection->append('user', 'relatedUser2');
+        $collection->append('user', 'relatedUser3');
+        $collection->append('relatedUser3', 'user');
+        $collection->append('relatedUser2', 'user');
+        $collection->append('user', 'relatedUser');
+        $collection->append('relatedUser2', 'relatedUser3');
+        $collection->append('relatedUser3', 'relatedUser2');
+
+        $this->assertEquals($collection->getAll(), [
+            'user' => ['relatedUser', 'relatedUser2', 'relatedUser3'],
+            'relatedUser' => ['user', 'relatedUser2', 'relatedUser3'],
+            'relatedUser2' => ['user', 'relatedUser', 'relatedUser3'],
+            'relatedUser3' => ['user', 'relatedUser', 'relatedUser2'],
+        ]);
+    }
+
+    public function testAppendingViaChilds()
+    {
+        $collection = new RelatedUsersCollection();
+
+        $collection->append('user', 'relatedUser');
+        $collection->append('relatedUser2', 'user');
+        $collection->append('relatedUser3', 'user');
+
+        $this->assertEquals($collection->getAll(), [
+            'user' => ['relatedUser', 'relatedUser2', 'relatedUser3'],
+            'relatedUser' => ['user', 'relatedUser2', 'relatedUser3'],
+            'relatedUser2' => ['user', 'relatedUser', 'relatedUser3'],
+            'relatedUser3' => ['user', 'relatedUser', 'relatedUser2'],
+        ]);
+    }
+
+    public function testGettingForAppendingViaChilds()
+    {
+        $collection = new RelatedUsersCollection();
+
+        $collection->append('user', 'relatedUser');
+        $collection->append('relatedUser2', 'user');
+        $collection->append('relatedUser3', 'user');
+
+        $this->assertEquals(
+            $collection->getByUserId('user'),
+            ['relatedUser', 'relatedUser2', 'relatedUser3']
+        );
+
+        $this->assertEquals(
+            $collection->getByUserId('relatedUser'),
+            ['user', 'relatedUser2', 'relatedUser3']
+        );
+
+        $this->assertEquals(
+            $collection->getByUserId('relatedUser2'),
+            ['user', 'relatedUser', 'relatedUser3'],
+        );
+
+        $this->assertEquals(
+            $collection->getByUserId('relatedUser3'),
+            ['user', 'relatedUser', 'relatedUser2']
+        );
+    }
+
+    public function testAppendingWithParallelRelatedUsers()
+    {
+        $collection = new RelatedUsersCollection();
+
+        $collection->append('user', 'relatedUser');
+        $collection->append('relatedUser2', 'user');
+        $collection->append('relatedUser3', 'user');
+
+        $collection->append('user2', 'user2Related');
+        $collection->append('user2', 'user2Related2');
+        $collection->append('user2', 'user2Related3');
+
+        $this->assertEquals($collection->getAll(), [
+            'user' => ['relatedUser', 'relatedUser2', 'relatedUser3'],
+            'relatedUser' => ['user', 'relatedUser2', 'relatedUser3'],
+            'relatedUser2' => ['user', 'relatedUser', 'relatedUser3'],
+            'relatedUser3' => ['user', 'relatedUser', 'relatedUser2'],
+
+            'user2' => ['user2Related', 'user2Related2', 'user2Related3'],
+            'user2Related' => ['user2', 'user2Related2', 'user2Related3'],
+            'user2Related2' => ['user2', 'user2Related', 'user2Related3'],
+            'user2Related3' => ['user2', 'user2Related', 'user2Related2'],
+        ]);
+    }
+
+    public function testAppendingWithParallelRelatedUsersMerge()
+    {
+        $collection = new RelatedUsersCollection();
+
+        $collection->append('user', 'relatedUser');
+        $collection->append('relatedUser2', 'user');
+        $collection->append('relatedUser3', 'user');
+
+        $collection->append('user2', 'user2Related');
+        $collection->append('user2', 'user2Related2');
+        $collection->append('user2', 'user2Related3');
+
+        $collection->append('user', 'user2');
+
+
+        $this->assertEquals($collection->getAll(), [
+            'user' => [
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+            'relatedUser' => [
+                'user',
+                'relatedUser2',
+                'relatedUser3',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+            'relatedUser2' => [
+                'user',
+                'relatedUser',
+                'relatedUser3',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+            'relatedUser3' => [
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+
+            'user2' => [
+                'user2Related',
+                'user2Related2',
+                'user2Related3',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+            'user2Related' => [
+                'user2',
+                'user2Related2',
+                'user2Related3',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+            'user2Related2' => [
+                'user2',
+                'user2Related',
+                'user2Related3',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+            'user2Related3' => [
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+        ]);
+    }
+
+    public function testAppendingWithParallelRelatedUsersMergeDirection2()
+    {
+        $collection = new RelatedUsersCollection();
+
+        $collection->append('user', 'user2');
+
+        $collection->append('user', 'relatedUser');
+        $collection->append('relatedUser2', 'user');
+        $collection->append('relatedUser3', 'user');
+
+        $collection->append('user2', 'user2Related');
+        $collection->append('user2', 'user2Related2');
+        $collection->append('user2', 'user2Related3');
+
+        $expected = [
+            'user' => [
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+            'relatedUser' => [
+                'user',
+                'relatedUser2',
+                'relatedUser3',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+            'relatedUser2' => [
+                'user',
+                'relatedUser',
+                'relatedUser3',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+            'relatedUser3' => [
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user2Related3'
+            ],
+
+            'user2' => [
+                'user2Related',
+                'user2Related2',
+                'user2Related3',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+            'user2Related' => [
+                'user2',
+                'user2Related2',
+                'user2Related3',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+            'user2Related2' => [
+                'user2',
+                'user2Related',
+                'user2Related3',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+            'user2Related3' => [
+                'user2',
+                'user2Related',
+                'user2Related2',
+                'user',
+                'relatedUser',
+                'relatedUser2',
+                'relatedUser3',
+            ],
+        ];
+
+        foreach ($expected as $expectedByUserId => $expectedValue) {
+            $actual = $collection->getByUserId($expectedByUserId);
+
+            sort($expectedValue);
+            sort($actual);
+
+            $this->assertEquals($expectedValue, $actual);
+        }
+    }
+}
